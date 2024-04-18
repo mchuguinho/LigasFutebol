cb46b3f525381498a177ff012197ae391d624092
shoteose <paulomnovo02@gmail.com>
2024-04-18 18:07:55 +0100

    ALT SHIFT F BEST TOOL EVER

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.getElementById("login-form").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Recebe o email e password do formulário
@@ -7,28 +7,28 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 
     // Carrega o ficheiro JSON com os dados de utilizadores
     fetch("../js/users.json")
-    .then(response => response.json())
-    .then(data => {
-        // Verifica se os dados recebidos do formulário correspondem aos do ficheiro JSON
-        var user = data.users.find(u => u.email === email && u.password === password);
-        if (user) {
-            //Criação de um Toast
-            Toastify({
-                text: "Utilizador logado com sucesso!",
-                duration: 3000,
-                close: true,
-                gravity: "top",
-                backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
-                callback: function() {
-                    window.location.href = "index.html";
-                }
-            }).showToast();
-        } else {
-            alert("Email ou password inválida. Tente outra vez!");
-        }
-    })
-    .catch(error => {
-        console.error("Error loading user data:", error);
-        alert("An error occurred. Please try again later.");
-    });
+        .then(response => response.json())
+        .then(data => {
+            // Verifica se os dados recebidos do formulário correspondem aos do ficheiro JSON
+            var user = data.users.find(u => u.email === email && u.password === password);
+            if (user) {
+                //Criação de um Toast
+                Toastify({
+                    text: "Utilizador logado com sucesso!",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top",
+                    backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+                    callback: function () {
+                        window.location.href = "index.html";
+                    }
+                }).showToast();
+            } else {
+                alert("Email ou password inválida. Tente outra vez!");
+            }
+        })
+        .catch(error => {
+            console.error("Error loading user data:", error);
+            alert("An error occurred. Please try again later.");
+        });
 });
--- js/registo.js
+++ js/registo.js
@@ -1,4 +1,4 @@
-document.getElementById("registo-form").addEventListener("submit", function(event) {
+document.getElementById("registo-form").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
 
     // vou buscar os valores
@@ -9,37 +9,37 @@ document.getElementById("registo-form").addEventListener("submit", function(even
     var apelido = document.getElementById("apelido").value;
 
     //verifico se as passes estão iguais
-        if(password != passwordC){
+    if (password != passwordC) {
 
-            alert("As palavras passes são diferentes!!")
-    
-        }else{
-    
-            
-    
-            // Load the JSON file containing existing user data (if any)
-            fetch("../js/users.json")
+        alert("As palavras passes são diferentes!!")
+
+    } else {
+
+
+
+        // Load the JSON file containing existing user data (if any)
+        fetch("../js/users.json")
             .then(response => response.json())
             .then(data => {
 
                 // verificar se já existe algum utilizador
                 var existeAlgum = data.users.find(u => u.email === email);
-                
+
 
                 if (existeAlgum) {
                     alert("Já existe um utilizador com esse email! Tente dar login!");
-                    
-                    window.location.href="./login.html";
+
+                    window.location.href = "./login.html";
                 } else {
 
                     // cria variavel com dados do json
                     var usersL = data.users;
 
                     //verifica a quantidade de users e adiciona mais 1 para criar o id superior
-                    var counterId= usersL.length + 1;
-     
+                    var counterId = usersL.length + 1;
+
                     //crio o user
-                    var user = { email: email, password: password, nome: nome,apelido: apelido, id: counterId};
+                    var user = { email: email, password: password, nome: nome, apelido: apelido, id: counterId };
                     console.log(user);
                     // adiciono user
                     data.users.push(user);
@@ -53,17 +53,17 @@ document.getElementById("registo-form").addEventListener("submit", function(even
                         body: JSON.stringify(data)
                     })
 
-                    .then(() => {
-                        alert("Registado com Sucesso!");
-                        window.location.href = "login.html";
-                    })
+                        .then(() => {
+                            alert("Registado com Sucesso!");
+                            window.location.href = "login.html";
+                        })
 
-                    .catch(error => {
-                        console.error("Erro ao registar:", error);
-                        alert("Ocurreu um erro!! Tente de novo! :c");
-                    });
+                        .catch(error => {
+                            console.error("Erro ao registar:", error);
+                            alert("Ocurreu um erro!! Tente de novo! :c");
+                        });
                 }
-            })  
+            })
     }
-    
-    });
+
+});
